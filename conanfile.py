f4ce3187fed96022eae403a3d828561e747ce9f3
danyanyam <dvbuchko@edu.hse.ru>
2022-09-24 15:05:36 +0300

    feature: gens

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class Example(ConanFile):
     name = "Example"
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package"
+    generators = "cmake", "cmake_find_package", "virtualenv", "virtualrunenv"
     requires = ["boost/1.71.0",
                 "nlohmann_json/3.11.2",
                 "openssl/1.1.1q",
